73f6301771ba8daf4ed967e1e6f6dc50a6ba7c6f
Suarez <2511130333@undc.edu.pe>
2026-09-16 17:18:39 -0500

    js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 console.log("Hola Cladio");
 alert("Hola desde scipt.js");
+// Muestra el modal al cargar la página web
+window.onload = function() {
+    document.getElementById("miModal").style.display = "block";
+};
+
+// Oculta el modal con la X o el botón Entendido
+function cerrarModal() {
+    document.getElementById("miModal").style.display = "none";
+}
